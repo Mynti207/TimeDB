1f304130bdcd016c3283cd39539bd8224824f813
nicodri <nicodrizard@hotmail.com>
2016-02-08 16:10:45 -0500

    string representation

--- src/TimeSeries.py
+++ src/TimeSeries.py
@@ -1,22 +1,37 @@
 # stores TimeSeries class
 
+
 class TimeSeries:
 
-	# initialize TimeSeries with a sequence object
-	def __init__(self, seq):
-		self.__series = seq
+    # initialize TimeSeries with a sequence object
+    def __init__(self, seq):
+        self.__series = seq
 
+    @property
+    def series(self):
+        return self.__series
 
-	@property
-	def series(self):
-	    return self.__series
+    def __len__(self):
+        return len(self.series)
 
-	def __len__(self):
-		return len(self.series)
+    def __getitem__(self, key):
+        return self.__series[key]
 
-	def __getitem__(self, key):
-		return self.__series[key]
+    def __setitem__(self, key, item):
+        self.__series[key] = item
 
-	def __setitem__(self, key, item):
-		self.__series[key] = item
+    def __str__(self):
+        n = len(self)
+        if n > 5:
+            return('Length: {} \n[{}, ..., {}]'.format(n, self[0], self[-1]))
+        else:
+            return(str(self.__series))
+
+    def __repr__(self):
+        n = len(self)
+        if n > 5:
+            res = '[{}, ..., {}]'.format(self[0], self[-1])
+        else:
+            res = str(self.__series)
+        return 'TimeSeries({})'.format(res)
 
--- src/main.py
+++ src/main.py
@@ -9,36 +9,64 @@ from TimeSeries import *
 
 # test cases
 def testBasicFunctionality():
-	
-	# create simple numpy sequence
-	seq = np.arange(0, 10)
 
-	ts = TimeSeries(seq)
+    # create simple numpy sequence
+    seq = np.arange(0, 10)
 
-	# get item
-	if ts[1] != 1:
-		return False
+    ts = TimeSeries(seq)
 
-	# set item
-	ts[1] = 0
-	if ts[1] != 0:
-		return False
+    # get item
+    if ts[1] != 1:
+        return False
 
-	# length
-	if len(ts) != 10:
-		return False
-	return True
+    # set item
+    ts[1] = 0
+    if ts[1] != 0:
+        return False
+
+    # length
+    if len(ts) != 10:
+        return False
+    return True
+
 
 # routine to perform testcases
+def testStringRepresentation():
+    # create simple numpy sequence
+    seq_short = np.arange(0, 5)
+    ts_short = TimeSeries(seq_short)
+
+    seq_large = np.arange(0, 10)
+    ts_large = TimeSeries(seq_large)
+
+    string_ts_short = str(ts_short)
+    string_ts_large = str(ts_large)
+
+    if string_ts_short != '[0 1 2 3 4]':
+        return False
+
+    if string_ts_large != 'Length: {} \n[0, ..., 9]'.format(len(ts_large)):
+        return False
+
+    print('Print statement on short list: \n{}'.format(string_ts_short))
+    print('Print statement on large list: \n{}'.format(string_ts_large))
+    return True
+
+
 def runTestCases():
-	print('running test cases...')
-	if not testBasicFunctionality():
-		print('test case basic functionality failed!')
-	else:
-		print('basic functionality, pass')
+    print('running test cases...')
+    if not testBasicFunctionality():
+        print('test case basic functionality failed!')
+    else:
+        print('basic functionality, pass')
+    if not testStringRepresentation():
+        print('test case string representation failed!')
+    else:
+        print('string representation, pass')
+
 
 def main():
-	runTestCases()
+    runTestCases()
 
 if __name__ == "__main__":
     main()
