aaa922b466e8eb4f9e668bb6ea2c69815ca4500b
nicodri <nicodrizard@hotmail.com>
2016-02-10 23:33:15 -0500

    fix

--- src/ArrayTimeSeries.py
+++ src/ArrayTimeSeries.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from TimeSeries import *
 
 
@@ -5,5 +7,5 @@ class ArrayTimeSeries(TimeSeries):
     """docstring for ArrayTimeSeries"""
     def __init__(self, seq):
         super(ArrayTimeSeries, self).__init__(seq)
-        self.__series = seq
+        self.__series = np.array(seq)
         
