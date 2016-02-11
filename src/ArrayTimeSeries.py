8ca72e308e77ab20b372d8b4c6ba6a664b63e248
nicodri <nicodrizard@hotmail.com>
2016-02-10 19:29:19 -0500

    arraytimeseries

---
+++ src/ArrayTimeSeries.py
@@ -0,0 +1,6 @@
+class ArrayTimeSeries(TimeSeries):
+    """docstring for ArrayTimeSeries"""
+    def __init__(self, arg):
+        super(ArrayTimeSeries, self).__init__()
+        self.arg = arg
+        
